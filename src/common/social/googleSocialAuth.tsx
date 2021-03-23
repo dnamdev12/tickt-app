d8517a2bc890b9330f904cd8bb3fbdc253d03e2a
Rahul Deshwal <rahul.deshwal@appinventiv>
2021-03-23 15:37:23 +0530

    build added

---
+++ src/common/social/googleSocialAuth.tsx
@@ -0,0 +1,11 @@
+import {GoogleLogin} from 'react-google-login';
+
+const GoogleSocialAuth = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GoogleSocialAuth
