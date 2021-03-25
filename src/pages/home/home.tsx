97c761ef6aa38904f027bace9ea831c58694ee61
nivedita.kumari@appinventiv.com <nivedita.kumari@appinventiv.com>
2021-03-25 12:40:19 +0530

    Add/choose qualification UI

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,6 +11,10 @@ import bell from '../../assets/images/ic-notification.png';
 import dummy from '../../assets/images/u_placeholder.jpg';
 import profile from '../../assets/images/ic-profile.png';
 import uc from '../../assets/images/uc.png';
+import Searchicon from "../../assets/images/main-search.png";
+import search from "../../assets/images/ic-search.png";
+import Location from "../../assets/images/ic-location.png";
+import bannerimg from '../../assets/images/home-banner.png'
 
 const Home = () => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -97,16 +101,74 @@ const Home = () => {
                 </div>
             </header>
 
-           
-            <div className="custom_container">
+
+            {/* <div className="custom_container">
                 <div className="under_construction_wrap">
                     <figure className="constrction_img">
                         <img src={uc} alt="coming soon" />
                     </figure>
                     <h2>This Page is under construction. Please come back later.</h2>
                 </div>
+            </div> */}
+
+
+            <div className="home_banner">
+                <figure className="banner_img">
+                    <img src={bannerimg} alt="bannerimg" />
+                    <div className="banner_container">
+                        <div className="home_search">
+                            <button className="modal_srch_close">
+                                <img src="assets/images/close 1.png" alt="close" />
+                            </button>
+                            <form className="search_wrapr">
+                                <ul>
+                                    <li className="categ_box">
+                                        <div className="text_field">
+                                            <input type="text" placeholder="Who are you searching for?" />
+                                            <div className="border_eff"></div>
+                                            <span className="detect_icon"> </span>
+                                            <span className="detect_icon_ltr">
+                                                <img src={Searchicon} alt="search" />
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li className="loc_box">
+                                        <div className="text_field">
+                                            <div>
+                                                <input type="text" placeholder="Where?" className="line-1" />
+                                                <span className="detect_icon_ltr">
+                                                    <img src={Location} alt="location" />
+                                                </span>
+                                                <span className="detect_icon" >
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="custom_date_range">
+                                            <div className="text_field">
+                                                <span className="detect_icon_ltr calendar"></span>
+                                                <input type="text" placeholder="When?" />
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <div className="search_btn">
+                                        <button type="button" className="fill_btn">
+                                            <img src={search} alt="search" />
+                                        </button>
+                                    </div>
+                                </ul>
+                            </form>
+                        </div>
+                        <div className="text-center">
+                            <h1 className="heading">Your local network</h1>
+                            <p className="commn_para">Connect with Tradies in your area</p>
+                            <a className="fill_btn view-btn">View More</a>
+                        </div>
+                    </div>
+                </figure>
             </div>
-            
+
         </div>
     )
 }
