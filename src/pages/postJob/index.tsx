d03c8b25e71e4a1f4cedf8de07d07693b2bacaa5
Rahul Deshwal <rahul.deshwal@appinventiv>
2021-04-01 18:43:53 +0530

    post job route added

---
+++ src/pages/postJob/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostNewJob from './components/postNewJob'
+
+const PostJob = () => {
+    return (
+        <div>
+            <PostNewJob />
+        </div>
+    )
+}
+
+export default PostJob
