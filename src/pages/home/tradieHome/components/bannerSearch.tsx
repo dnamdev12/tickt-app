2cac89d6cddaf0d90bbffc924415b36fe2d3811b
Rahul Deshwal <rahul.deshwal@appinventiv>
2021-04-14 10:45:14 +0530

    tradieHome banner initial setup

--- src/pages/home/tradieHome/components/bannerSearch.tsx
+++ src/pages/home/tradieHome/components/bannerSearch.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import regex from '../../../../utils/regex';
 
-
 import colorLogo from '../../../../assets/images/ic-logo-yellow.png';
-import dummy from '../../../assets/images/u_placeholder.jpg';
+import dummy from '../../../../assets/images/u_placeholder.jpg';
 import Searchicon from "../../../../assets/images/main-search.png";
 import search from "../../../../assets/images/ic-search.png";
 import Location from "../../../../assets/images/ic-location.png";
 import cross from "../../../../assets/images/close-black.png";
 import bannerimg from '../../../../assets/images/home-banner.png';
+import icgps from "../../../../assets/images/ic-gps.png";
 import residential from "../../../assets/images/ic-residential.png";
 import industrial from "../../../assets/images/ic-money.png";
 import contracted from "../../../assets/images/ic-contracted.png";
@@ -18,36 +18,69 @@ import hourlyRate from "../../../assets/images/ic-clock.png";
 
 const BannerSearch = (props: any) => {
     const [stateData, setStateData] = useState<any>({
-        city: '',
-        cityId: null,
+        searchedJob: '',
+        tradeId: '',
+        specializationId: '',
+        searchedJobId: null,
         location: '',
     });
-    const [inputFocus, setInputFocus] = useState<boolean>(false)
+    const [inputFocus1, setInputFocus1] = useState<boolean>(false)
+    const [inputFocus2, setInputFocus2] = useState<boolean>(false)
+    const [inputFocus3, setInputFocus3] = useState<boolean>(false)
 
-    const checkInputValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let valid = true;
-        const alphaRegex = new RegExp(regex.alphaSpecial)
-        return valid = alphaRegex.test(e.target.value)
+    useEffect(() => {
+        window.addEventListener('mousedown', handleClicked)
+
+        // navigator.geolocation.getCurrentPosition(function (position) {
+        //     console.log("Latitude is :", position.coords.latitude);
+        //     console.log("Longitude is :", position.coords.longitude);
+        // });
+
+        return () => {
+            window.removeEventListener('mousedown', handleClicked)
+        }
+    }, [])
+
+    const handleClicked = (event: any) => {
+        if (document.getElementById("recent-job-search-div") && (!document.getElementById("text-field-div")?.contains(event.target) && !document.getElementById("recent-job-search-div")?.contains(event.target))) {
+            console.log("handleClicked1")
+            setInputFocus1(false)
+        }
+        if (document.getElementById("custom-location-search-div") && (!document.getElementById("location-text-field-div")?.contains(event.target) && !document.getElementById("custom-location-search-div")?.contains(event.target))) {
+            console.log("handleClicked2")
+            setInputFocus2(false)
+        }
     }
 
-    const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checkInputValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const alphaRegex = new RegExp(regex.alphaSpecial)
+        return alphaRegex.test(e.target.value)
+    }
+
+    const handleJobChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (checkInputValidation(e)) {
             e.target.value.length > 0 && props.getSearchJobList(e.target.value)
-            setStateData((prevData: any) => ({ ...prevData, city: e.target.value }))
+            setStateData((prevData: any) => ({ ...prevData, searchedJob: e.target.value }))
         }
     }
 
     const cleanInputData = (item: string) => {
-        console.log(item, "clean INput")
-        setStateData((prevData: any) => ({ ...prevData, [item]: '' }))
+        setStateData((prevData: any) => ({ ...prevData, [item]: '', tradeId: '' }))
+        // setInputFocus1(false)
+    }
+
+    const searchedJobClicked = (item: any) => {
+        console.log(item, "item recent")
+        setStateData((prevData: any) => ({ ...prevData, searchedJob: item.name, tradeId: item._id, specializationId: item.specializationsId }));
+        setInputFocus1(false);
     }
 
     const recentJobSearches = () => {
         return (
-            <div className="custom_autosuggestion">
+            <div className="custom_autosuggestion" id="recent-job-search-div">
                 <span className="sub_title">Recent searches</span>
                 <div className="flex_row recent_search">
-                    <div className="flex_col_sm_4">
+                    <div className="flex_col_sm_4" onClick={() => searchedJobClicked({ name: 'Campervans', _id: 'dhc78hi237hiu', specializationId: 'jey89ehe997yyh' })}>
                         <div className="autosuggestion_icon card history">
                             <span>Campervans</span>
                             <span className="name">Vehicles</span>
@@ -69,10 +102,10 @@ const BannerSearch = (props: any) => {
         console.log(props.searchJobListData, "props.searchJobListData")
         return (
             props.searchJobListData?.length ? <div className="custom_autosuggestion">
-                <div className="flex_row recent_search">
+                <div className="flex_col recent_search">
                     {props.searchJobListData?.map((item: any) => {
                         return (
-                            <div className="flex_col_sm_4">
+                            <div className="flex_col_sm_4" onClick={() => searchedJobClicked(item)}>
                                 <div className="autosuggestion_icon card history">
                                     <span>{item.name}</span>
                                     <span className="name">{item.trade_name}</span>
@@ -84,6 +117,8 @@ const BannerSearch = (props: any) => {
             </div> : <p>No Result Found!</p>
         )
     }
+
+    console.log("bannerSearch ==> ", stateData, inputFocus1)
 
     return (
         <div className="home_banner">
@@ -97,31 +132,66 @@ const BannerSearch = (props: any) => {
                         <form className="search_wrapr">
                             <ul>
                                 <li className="categ_box">
-                                    <div className="text_field">
-                                        <input type="text" placeholder="What jobs are you after?" value={stateData.city} onChange={handleCityChange} onFocus={() => setInputFocus(true)} onBlur={() => setInputFocus(false)} />
+                                    <div className="text_field" id="text-field-div">
+                                        <input type="text" placeholder="What jobs are you after?" value={stateData.searchedJob} onChange={handleJobChange} onFocus={() => setInputFocus1(true)} />
                                         <div className="border_eff"></div>
                                         <span className="detect_icon_ltr">
                                             <img src={Searchicon} alt="search" />
                                         </span>
-                                        {stateData.city.length > 0 &&
+                                        {stateData.searchedJob && inputFocus1 &&
                                             <span className="detect_icon" >
-                                                <img src={cross} alt="cross" onClick={() => cleanInputData('city')} />
+                                                <img src={cross} alt="cross" onClick={() => cleanInputData('searchedJob')} />
                                             </span>}
                                     </div>
                                 </li>
+                                {!stateData.searchedJob && inputFocus1 && recentJobSearches()}
+                                {stateData.searchedJob && inputFocus1 && renderJobResult()}
                                 <li className="loc_box">
-                                    <div className="text_field">
+                                    <div className="text_field" id="location-text-field-div">
                                         <div>
-                                            <input type="text" placeholder="Where?" className="line-1" />
+                                            <input type="text" placeholder="Where?" className="line-1" value={stateData.location} onChange={handleJobChange} onFocus={() => setInputFocus2(true)} />
                                             <span className="detect_icon_ltr">
                                                 <img src={Location} alt="location" />
                                             </span>
-                                            <span className="detect_icon" >
+                                            {stateData.location && inputFocus2 && <span className="detect_icon" >
                                                 <img src={cross} alt="cross" />
-                                            </span>
+                                            </span>}
                                         </div>
                                     </div>
                                 </li>
+                                {inputFocus2 &&
+                                    <div className="custom_autosuggestion location" id="custom-location-search-div">
+                                        <button className="location-btn">
+                                            <span className="gps_icon">
+                                                <img src={icgps} />
+                                            </span> Use my current location
+                                        </button>
+                                        <span className="sub_title ">Recent searches</span>
+                                        {/* <span className="blocked_note">
+                                            You have blocked your location.
+                                            To use this, change your location settings in browser.
+                                        </span> */}
+                                        <div className="flex_row recent_search auto_loc">
+                                            <div className="flex_col_sm_4">
+                                                <div className="autosuggestion_icon card loc">
+                                                    <span >Dummy location</span>
+                                                    <span className="name">Noida, Uttar Pradesh, India</span></div></div>
+
+                                            <div className="flex_col_sm_4">
+                                                <div className="autosuggestion_icon card loc">
+                                                    <span >Unnamed Road</span>
+                                                    <span className="name"> Bichua, Madhya Pradesh 487001, India</span>
+                                                </div>
+                                            </div>
+                                            <div className="flex_col_sm_4">
+                                                <div className="autosuggestion_icon card loc">
+                                                    <span >4 shantanu banlows rajpath club ni same</span>
+                                                    <span className="name"> Narolgam, Ellisbridge, Ahmedabad, Gujarat 380006, India</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
                                 <li>
                                     <div className="custom_date_range">
                                         <div className="text_field">
@@ -137,44 +207,6 @@ const BannerSearch = (props: any) => {
                                 </div>
                             </ul>
                         </form>
-
-                        {stateData.city.length === 0 && inputFocus && recentJobSearches()}
-                        {stateData.city.length > 0 && renderJobResult()}
-
-                        {/* Location recent search */}
-                        {/* <div className="custom_autosuggestion location">
-                                <button className="location-btn">
-                                    <span className="gps_icon">
-                                        <img src={icgps} />
-                                    </span> Use my current location
-                                </button>
-                                <span className="sub_title ">Recent searches</span>
-                                 <span className="blocked_note">
-                              You have blocked your location.
-                              To use this, change your location settings in browser.
-                              </span> 
-                                <div className="flex_row recent_search auto_loc">
-                                    <div className="flex_col_sm_4">
-                                        <div className="autosuggestion_icon card loc">
-                                            <span >Dummy location</span>
-                                            <span className="name">Noida, Uttar Pradesh, India</span></div></div>
-
-                                    <div className="flex_col_sm_4">
-                                        <div className="autosuggestion_icon card loc">
-                                            <span >Unnamed Road</span>
-                                            <span className="name"> Bichua, Madhya Pradesh 487001, India</span>
-                                        </div>
-                                    </div>
-                                    <div className="flex_col_sm_4">
-                                        <div className="autosuggestion_icon card loc">
-                                            <span >4 shantanu banlows rajpath club ni same</span>
-                                            <span className="name"> Narolgam, Ellisbridge, Ahmedabad, Gujarat 380006, India</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> */}
-                        {/* Location recent search close*/}
-
                     </div>
                     <div className="text-center">
                         <h1 className="heading text_effect">See all around me</h1>
