d1671254e86d0e8a67aaf82fd37254111cf5d603
nivedita.kumari@appinventiv.com <nivedita.kumari@appinventiv.com>
2021-05-10 09:54:31 +0530

    checkout

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import menu from '../../assets/images/ic-filter-unselected.png';
 import remove from '../../assets/images/close 1.png';
+import dummy from '../../assets/images/u_placeholder.jpg';
 
 export default function NotFound() {
     return (
@@ -33,12 +34,87 @@ export default function NotFound() {
         //                         <a className="icon star active">Active Jobs</a>
         //                     </li>
         //                     <li>
-        //                         <a className="icon applied">Applied jobs</a>
+        //                         <a className="icon applied">Applied jobs
+        //                             <span className="badge_count">5</span>
+        //                         </a>
         //                     </li>
         //                     <li>
         //                         <a className="icon past">Past jobs</a>
         //                     </li>
+        //                     <li>
+        //                         <a className="icon new">New jobs</a>
+        //                     </li>
+        //                     <li>
+        //                         <a className="icon approved">Approved milestones</a>
+        //                     </li>
         //                 </ul>
+        //             </div>
+        //         </div>
+        //         <div className="detail_col">
+        //             <div className="flex_row tradies_row">
+        //                 <div className="flex_col_sm_6">
+        //                     <div className="tradie_card">
+        //                         <a href="javascript:void(0)" className="more_detail circle"></a>
+        //                         <div className="user_wrap">
+        //                             <figure className="u_img">
+        //                                 <img src={dummy} alt="traide-img" />
+        //                             </figure>
+        //                             <div className="details">
+        //                                 <span className="name">Wire up circuit box</span>
+        //                             </div>
+        //                         </div>
+        //                         <div className="job_info">
+        //                             <ul>
+        //                                 <li className="icon clock">32 minutes ago</li>
+        //                                 <li className="icon dollar">$250 p/h</li>
+        //                                 <li className="icon location">Melbourne CBD</li>
+        //                                 <li className="icon calendar">4 days </li>
+        //                             </ul>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //                 <div className="flex_col_sm_6">
+        //                     <div className="tradie_card">
+        //                         <a href="javascript:void(0)" className="more_detail circle"></a>
+        //                         <div className="user_wrap">
+        //                             <figure className="u_img">
+        //                                 <img src={dummy} alt="traide-img" />
+        //                             </figure>
+        //                             <div className="details">
+        //                                 <span className="name">Wire up circuit box</span>
+        //                             </div>
+        //                         </div>
+        //                         <div className="job_info">
+        //                             <ul>
+        //                                 <li className="icon clock">32 minutes ago</li>
+        //                                 <li className="icon dollar">$250 p/h</li>
+        //                                 <li className="icon location">Melbourne CBD</li>
+        //                                 <li className="icon calendar">4 days </li>
+        //                             </ul>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //                 <div className="flex_col_sm_6">
+        //                     <div className="tradie_card">
+        //                         <a href="javascript:void(0)" className="more_detail circle"></a>
+        //                         <div className="user_wrap">
+        //                             <figure className="u_img">
+        //                                 <img src={dummy} alt="traide-img" />
+        //                             </figure>
+        //                             <div className="details">
+        //                                 <span className="name">Wire up circuit box</span>
+        //                             </div>
+        //                         </div>
+        //                         <div className="job_info">
+        //                             <ul>
+        //                                 <li className="icon clock">32 minutes ago</li>
+        //                                 <li className="icon dollar">$250 p/h</li>
+        //                                 <li className="icon location">Melbourne CBD</li>
+        //                                 <li className="icon calendar">4 days </li>
+        //                             </ul>
+        //                         </div>
+        //                     </div>
+        //                 </div>
         //             </div>
         //         </div>
         //     </div>
