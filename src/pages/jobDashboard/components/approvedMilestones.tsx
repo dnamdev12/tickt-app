da55566ba40f61aff0d4132cb7504534fa23eaa6
Vrajpal Jhala <vrajpal@appinventiv.in>
2021-05-14 13:53:22 +0530

    fix: saga issue

--- src/pages/jobDashboard/components/approvedMilestones.tsx
+++ src/pages/jobDashboard/components/approvedMilestones.tsx
@@ -18,46 +18,48 @@ const ApprovedMilestones = ({ getApprovedMilestoneList, approvedMilestoneList }:
       {/* Approved Milestones */}
       <span className="sub_title">Approved Milestones</span>
       <div className="flex_row tradies_row">
-        <div className="flex_col_sm_6">
-          <div className="tradie_card">
-            <NavLink className="more_detail circle" to="/mark-milestone/1" />
-            <div className="user_wrap">
-              <figure className="u_img">
-                <img src={dummy} alt="traide-img" />
-              </figure>
-              <div className="details">
-                <span className="name">Wire up circuit box</span>
+        {approvedMilestoneList.map(({ jobId, tradeId, specializationId, tradeSelectedUrl, jobName, time, amount, locationName, durations }) => (
+          <div className="flex_col_sm_6">
+            <div className="tradie_card">
+              <NavLink to={`/job-details-page?jobId=${jobId}&tradeId=${tradeId}&specializationId=${specializationId}`} className="more_detail circle"></NavLink>
+              <div className="user_wrap">
+                <figure className="u_img">
+                  <img src={tradeSelectedUrl || dummy} alt="traide-img" />
+                </figure>
+                <div className="details">
+                  <span className="name">{jobName}</span>
+                </div>
               </div>
-            </div>
-            <div className="job_info">
-              <ul>
-                <li className="icon clock">32 minutes ago</li>
-                <li className="icon dollar">$250 p/h</li>
-                <li className="icon location line-1">Melbourne CBD</li>
-                <li className="icon calendar">4 days </li>
-              </ul>
-            </div>
-            <div className="job_progress_wrap" id="scroll-progress-bar">
-              <div className="progress_wrapper">
-                <span className="completed-digit" id="digit-progress">
-                  <b>Job Milestones 2</b> of 5
-                </span>
-                <span className="approval_info">
-                  <img src={approved} alt="icon" />
-                  Approved{' '}
-                </span>
-                <span className="progress_bar">
-                  <input
-                    className="done_progress"
-                    id="progress-bar"
-                    type="range"
-                    min="0"
-                  />
-                </span>
+              <div className="job_info">
+                <ul>
+                  <li className="icon clock">{time}</li>
+                  <li className="icon dollar">{amount}</li>
+                  <li className="icon location line-1">{locationName}</li>
+                  <li className="icon calendar">{durations}</li>
+                </ul>
+              </div>  
+              <div className="job_progress_wrap" id="scroll-progress-bar">
+                <div className="progress_wrapper">
+                  <span className="completed-digit" id="digit-progress">
+                    <b>Job Milestones 2</b> of 5
+                  </span>
+                  <span className="approval_info">
+                    <img src={approved} alt="icon" />
+                    Approved
+                  </span>
+                  <span className="progress_bar">
+                    <input
+                      className="done_progress"
+                      id="progress-bar"
+                      type="range"
+                      min="0"
+                    />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
       {/* Approved Milestones close */}
     </>
