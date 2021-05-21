457e9e8144c1d505051d11f7417499525d5042bf
nivedita.kumari@appinventiv.com <nivedita.kumari@appinventiv.com>
2021-05-21 12:31:14 +0530

    milestone details UI

--- src/pages/builderJobDasboard/jobDashboard.tsx
+++ src/pages/builderJobDasboard/jobDashboard.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Route, Switch, useHistory } from 'react-router-dom';
 import menu from '../../assets/images/menu-line-blue.png';
 import close from '../../assets/images/ic-cancel-blue.png';
-import dummy from '../../assets/images/u_placeholder.jpg';
+import media from '../../assets/images/portfolio-placeholder.jpg';
 import approved from '../../assets/images/approved.png';
 import tradieListData from '../shared/tradieListData';
 import rateStar from '../../assets/images/ic-star-fill.png';
@@ -78,7 +78,7 @@ const JobDashboard = ({
 
         if (jobType === 'applicantList') {
             if (getnewJobApplicationListBuilder) {
-                console.log({page, jobId});
+                console.log({ page, jobId });
                 getnewJobApplicationListBuilder(page, jobId);
             }
         }
@@ -182,7 +182,7 @@ const JobDashboard = ({
                         </div>
                     </div>
                     <div className="detail_col">
-                        {'Here!'}
+
                         {/* {jobType === 'active' && (
                             <ActiveJobsComponent
                                 dataItems={dataItems}
@@ -205,6 +205,49 @@ const JobDashboard = ({
                                 jobType={jobType}
                                 setJobLabel={setJobLabel}
                             />)} */}
+
+
+                        {/* Milestone details */}
+
+                        <div className="flex_row">
+                            <div className="flex_col_sm_8">
+                                <div className="relate">
+                                    <button className="back"></button>
+                                    <span className="xs_sub_title">Wire up circuit box</span>
+                                </div>
+                                <span className="sub_title">Milestone details</span>
+                                <span className="xs_sub_title">Circuit board wiring complete</span>
+                                <div className="upload_img_video">
+                                    <figure className="img_video">
+                                        <img src={media} alt="media" />
+                                        {/* <img src={close} alt="remove" className="remove" /> */}
+                                    </figure>
+                                    <figure className="img_video">
+                                        <img src={media} alt="media" />
+                                    </figure>
+                                    <figure className="img_video">
+                                        <img src={media} alt="media" />
+                                    </figure>
+                                </div>
+                                <div className="form_field">
+                                    <span className="xs_sub_title">Discription</span>
+                                    <p className="commn_para">Sparky wanted for a quick job to hook up two floodlights on the exterior of an apartment building to the main electrical grid. Current sparky away due to illness so need a quick replacement, walls are all prepped and just need lights wired. Can also provide free lunch on site and a bit of witty banter on request.</p>
+                                </div>
+
+                                <div className="form_field">
+                                    <span className="xs_sub_title">Hours worked in this milestone</span>
+                                    <span className="show_label">5 hours</span>
+                                </div>
+                                <button className="fill_btn full_btn">Approve</button>
+                                <button className="fill_grey_btn full_btn mt-16">Decline</button>
+                            </div>
+                        </div>
+
+                        {/* Milestone details close*/}
+
+
+
+
                     </div>
                 </div>
             </div>
