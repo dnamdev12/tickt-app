da407d244dbe22a998e78c0b41e944f63b91a800
nivedita.kumari@appinventiv.com <nivedita.kumari@appinventiv.com>
2021-05-24 13:51:48 +0530

    decline milestones UI

--- src/pages/builderJobDasboard/components/declineMilestone.tsx
+++ src/pages/builderJobDasboard/components/declineMilestone.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-
+import React from 'react';
+import media from '../../../assets/images/portfolio-placeholder.jpg';
+import close from '../../../assets/images/icon-close-1.png';
+import addMedia from "../../../assets/images/add-image.png";
 interface Props {
-    
+
 }
 
 const DeclineMilestone = (props: Props) => {
     return (
-        <div>
-            {'DeclineMilestone'}
+        <div className="flex_row">
+            <div className="flex_col_sm_7">
+                <div className="relate">
+                    <button className="back"></button>
+                    <span className="xs_sub_title">Job name</span>
+                </div>
+                <span className="sub_title">Decline milestone</span>
+                <p className="commn_para">Please write your reason for declining the milestone</p>
+                <div className="form_field">
+                    <label className="form_label">Your reason</label>
+                    <div className="text_field">
+                        <textarea placeholder="Your reason..."></textarea>
+                    </div>
+                    {/* <span className="char_count"></span> */}
+                </div>
+                <div className="upload_img_video">
+                    <figure className="img_video">
+                        <img src={media} alt="media" />
+                        <img src={close} alt="remove" className="remove" />
+                    </figure>
+                    <figure className="img_video">
+                        <img src={media} alt="media" />
+                    </figure>
+                    <label className="upload_media" htmlFor="upload_img_video">
+                        <img src={addMedia} alt="" />
+                    </label>
+                    <input
+                        type="file"
+                        accept="image/png,image/jpg,image/jpeg,.pdf, .doc, video/mp4, video/wmv, video/avi"
+                        style={{ display: "none" }}
+                        id="upload_img_video"
+                    />
+                </div>
+                <button className="fill_btn full_btn">Send</button>
+            </div>
         </div>
     )
 }
--- src/pages/builderJobDasboard/components/declineMilestoneSuccess.tsx
+++ src/pages/builderJobDasboard/components/declineMilestoneSuccess.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import templateImage from '../../../assets/images/job-complete-bg.png';
+
 
 interface Props {
     
@@ -6,9 +8,21 @@ interface Props {
 
 const DeclineMilestoneSuccess = (props: Props) => {
     return (
-        <div>
-            {'DeclineMilestoneSuccess'}
+        <div className="img_text_wrap">
+      <figure className="full_image">
+        <img src={templateImage} alt="template-image" />
+        <div className="short_info">
+          <div className="content">
+            <h1 className="title">Got it!</h1>
+            <span className="show_label">The tradesperson will review and get in touch with you. 
+            </span>
+            <div className="btn_wrapr">
+              <button className="fill_btn">OK</button>
+            </div>
+          </div>
         </div>
+      </figure>
+    </div>
     )
 }
 
