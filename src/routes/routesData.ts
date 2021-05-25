ca9416a3880570fc1dca88ed03c265ca15a17ec5
Rahul Deshwal <rahul.deshwal@appinventiv>
2021-05-25 18:42:57 +0530

    m5 routes commented

--- src/routes/routesData.ts
+++ src/routes/routesData.ts
@@ -100,11 +100,11 @@ const routes = [
         authRoute: false,
         privateRoute: true,
     },
-    {
-        name: 'builderinfo',
-        path: '/builder-info',
-        component: BuilderInfo,
-    },
+    // {
+    //     name: 'builderinfo',
+    //     path: '/builder-info',
+    //     component: BuilderInfo,
+    // },
     {
         name: 'tradieinfo',
         path: '/tradie-info',
@@ -119,7 +119,7 @@ const routes = [
             '/new-jobs',
             '/approved-milestones',
             '/mark-milestone',
-            '/review-builder',
+            // '/review-builder',
         ],
         component: JobDashboard,
         authRoute: false,
