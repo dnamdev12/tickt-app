2a35587afa36073c26088473e5ed07fada91cb2a
Hemant <hemant.8@appinventiv.com>
2021-06-01 14:34:12 +0530

    change url to QA

--- src/network/Urls.ts
+++ src/network/Urls.ts
@@ -1,5 +1,5 @@
-const ACTIVE_HOST = 'https://ticktdevapi.appskeeper.in';  // dev env
-// const ACTIVE_HOST = 'https://ticktqaapi.appskeeper.in';  // qa env
+// const ACTIVE_HOST = 'https://ticktdevapi.appskeeper.in';  // dev env
+const ACTIVE_HOST = 'https://ticktqaapi.appskeeper.in';  // qa env
 // const ACTIVE_HOST = 'https://ticktstgapi.appskeeper.in';  // stg env
 
 const versions = {
