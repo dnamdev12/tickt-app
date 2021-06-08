5f0838ba6663564e3feb7aaf99d7b8fe565a6769
Hemant <hemant.8@appinventiv.com>
2021-06-08 18:31:32 +0530

    toggle-modal vouchers

--- src/pages/tradieInfo/vouchers.tsx
+++ src/pages/tradieInfo/vouchers.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import dummy from '../../assets/images/u_placeholder.jpg';
 import vouch from '../../assets/images/ic-template.png';
+import Modal from '@material-ui/core/Modal';
 interface Props {
 
 }
@@ -9,7 +10,13 @@ interface State {
 }
 
 export default class Vouchers extends Component<Props, State> {
-    state = {}
+    state = {
+        toggle: false
+    }
+
+    toggleModal = () => {
+        this.setState({ toggle: !this.state.toggle });
+    }
 
     render() {
         return (
@@ -24,51 +31,63 @@ export default class Vouchers extends Component<Props, State> {
                                 </div>
                             </div>
                             <div className="flex_col_sm_6 text-right">
-                                <button className="fill_btn btn-effect add_vouch">+ Leave a voucher</button>
+                                <button 
+                                onClick={this.toggleModal}
+                                className="fill_btn btn-effect add_vouch">+ Leave a voucher</button>
                             </div>
                         </div>
-                        <div className="flex_row">
-                            <div className="flex_col_sm_3">
-                                <div className="review_card vouchers">
-                                    <div className="pic_shot_dtl">
-                                        <figure className="u_img">
-                                            <img src={dummy} alt="user-img" />
-                                        </figure>
-                                        <div className="name_wrap">
-                                            <span className="user_name" title="Mark Spencerman">Mark Spencerman</span>
-                                            <span className="date">November 2020</span>
+                        <Modal
+                            className="custom_modal"
+                            open={this.state.toggle}
+                            onClose={() => {
+                                this.toggleModal();
+                            }}
+                            aria-labelledby="simple-modal-title"
+                            aria-describedby="simple-modal-description"
+                        >
+                            <div className="flex_row">
+                                <div className="flex_col_sm_3">
+                                    <div className="review_card vouchers">
+                                        <div className="pic_shot_dtl">
+                                            <figure className="u_img">
+                                                <img src={dummy} alt="user-img" />
+                                            </figure>
+                                            <div className="name_wrap">
+                                                <span className="user_name" title="Mark Spencerman">Mark Spencerman</span>
+                                                <span className="date">November 2020</span>
+                                            </div>
+                                        </div>
+                                        <p className="commn_para" title="">I give a guarantee for the work of this builder. This is a vouch from our company.</p>
+                                        <div className="vouch">
+                                            <figure className="vouch_icon">
+                                                <img src={vouch} alt="vouch" />
+                                            </figure>
+                                            <a className="link">Vouch for John Oldman</a>
                                         </div>
                                     </div>
-                                    <p className="commn_para" title="">I give a guarantee for the work of this builder. This is a vouch from our company.</p>
-                                    <div className="vouch">
-                                        <figure className="vouch_icon">
-                                            <img src={vouch} alt="vouch" />
-                                        </figure>
-                                        <a className="link">Vouch for John Oldman</a>
+                                </div>
+                                <div className="flex_col_sm_3">
+                                    <div className="review_card vouchers">
+                                        <div className="pic_shot_dtl">
+                                            <figure className="u_img">
+                                                <img src={dummy} alt="user-img" />
+                                            </figure>
+                                            <div className="name_wrap">
+                                                <span className="user_name" title="Mark Spencerman">Mark Spencerman</span>
+                                                <span className="date">November 2020</span>
+                                            </div>
+                                        </div>
+                                        <p className="commn_para" title="">I give a guarantee for the work of this builder. This is a vouch from our company.</p>
+                                        <div className="vouch">
+                                            <figure className="vouch_icon">
+                                                <img src={vouch} alt="vouch" />
+                                            </figure>
+                                            <a className="link">Vouch for John Oldman</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex_col_sm_3"> 
-                                <div className="review_card vouchers">
-                                    <div className="pic_shot_dtl">
-                                        <figure className="u_img">
-                                            <img src={dummy} alt="user-img" />
-                                        </figure>
-                                        <div className="name_wrap">
-                                            <span className="user_name" title="Mark Spencerman">Mark Spencerman</span>
-                                            <span className="date">November 2020</span>
-                                        </div>
-                                    </div>
-                                    <p className="commn_para" title="">I give a guarantee for the work of this builder. This is a vouch from our company.</p>
-                                    <div className="vouch">
-                                        <figure className="vouch_icon">
-                                            <img src={vouch} alt="vouch" />
-                                        </figure>
-                                        <a className="link">Vouch for John Oldman</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        </Modal>
                     </div>
                 </div>
             </div>
