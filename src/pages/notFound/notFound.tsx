a0cb0446eeb08c9b6cfc8900d730da969ecc602b
nivedita.kumari@appinventiv.com <nivedita.kumari@appinventiv.com>
2021-06-24 18:36:29 +0530

    pull

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -28,16 +28,6 @@ export default function NotFound() {
 
     return (
 
-        // <div className="error_page_wrap">
-        //     <figure className="no_img">
-        //         <img src={notFound} alt="not-found" />
-        //     </figure>
-        //     <div className="content">
-        //         <h1>Page Not Found</h1>
-        //         <Link to="/" className="fill_btn full_btn btn-effect" title="Home">Back To Home</Link>
-        //     </div>
-        // </div>
-
 
 
         // -----My revenue list ------//
