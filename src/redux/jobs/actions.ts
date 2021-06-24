8686d7da6377c1bfe32cc0c4f6cb073e4b3f15cf
Hemant <hemant.8@appinventiv.com>
2021-06-24 10:03:24 +0530

    fixed action cancel-job

--- src/redux/jobs/actions.ts
+++ src/redux/jobs/actions.ts
@@ -451,7 +451,7 @@ export const tradielodgeDispute = async (data: any) => {
 
 export const CancelJob = async (data: any) => {
   setLoading(true);
-  const response: FetchResponse = await NetworkOps.postToJson(`${Urls.jobBuilder}canceljob`, data);
+  const response: FetchResponse = await NetworkOps.putToJson(`${Urls.jobBuilder}canceljob`, data);
   setLoading(false);
   if (response.status_code === 200) {
     setShowToast(true, response.message)
