237051971c3fb572b13bb1e16931422da3701e4a
Hemant <hemant.8@appinventiv.com>
2021-07-29 11:25:33 +0530

    fixed issues

--- src/pages/postJob/components/chooseTimingMileStone.tsx
+++ src/pages/postJob/components/chooseTimingMileStone.tsx
@@ -4,16 +4,15 @@ import React, { useState, useEffect } from 'react';
 // import bell from '../../../assets/images/ic-notification.png';
 // import dummy from '../../../assets/images/u_placeholder.jpg';
 // @ts-ignore
-import { DateRangePicker } from 'react-date-range';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-
-
+// import { DateRangePicker } from 'react-date-range';
+// import 'react-date-range/dist/styles.css'; // main style file
+// import 'react-date-range/dist/theme/default.css'; // theme css file
 
 // @ts-ignore
-// import { DateRangePicker } from '../../../plugins/react-date-range/dist/index';
-// import '../../../plugins/react-date-range/dist/styles.css';
-// import '../../../plugins/react-date-range/dist/theme/default.css';
+import { DateRangePicker } from '../../../plugins/react-date-range/dist/index';
+import '../../../plugins/react-date-range/dist/styles.css';
+import '../../../plugins/react-date-range/dist/theme/default.css';
+
 import moment from 'moment';
 import { setShowToast } from '../../../redux/common/actions';
 
