2f01c0f74492c47c4f685840903452af9f649014
Hemant Kumar <hemantwork05@gmail.com>
2021-09-21 12:59:47 +0530

    fixed doc viewer

--- src/common/DocViewer.js
+++ src/common/DocViewer.js
@@ -1,31 +1,49 @@
 import React from 'react';
-import DocViewer, { DocViewerRenderers , MSDocRenderer} from "react-doc-viewer";
-
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
 const DocViewerComponent = ({ src, width, height }) => {
     // https://docs.google.com/viewer?url=
-    console.log({ src, width, height })
+    let checkFormat = src;
+    let splitElements = checkFormat.split('.');
+    let extension = splitElements[splitElements?.length - 1];
+    let screenWidth = window.screen.width;
+    // console.log({ src, width, height, screenWidth })
     return (
-        <div>
-            <DocViewer
-                pluginRenderers={MSDocRenderer}
-                documents={[
-                    {
-                        uri: src
-                    },
-                ]} />
-            {/* <iframe
-                src={'https://docs.google.com/gview?url=' + src + '&embedded=true'}
-                sandbox='allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation'
-                allow="fullscreen"
-                allowFullScreen="allow" 
-                title="file"
-                width={width || '100%'}
-                style={{minHeight: height || '800px', maxHeight:'2000px'}}
-                // style={{ Height: '450px', maxHeight: '450px', overflowY: 'scroll' }}
-            ></iframe> */}
-        </div>
+        <DocViewer
+            pluginRenderers={DocViewerRenderers}
+            documents={[{ uri: src, fileType: extension }]}
+            style={{
+                height: height ? height : window.screen.height,
+                width: '100%',
+                paddingTop: width !== '100%' ? 80 : 0
+            }}
+            theme={{
+                disableThemeScrollbar: true,
+            }}
+            config={{
+                header: {
+                    disableHeader: true,
+                    disableFileName: true,
+                    retainURLParams: true,
+                }
+            }}
+        />
     )
+
+    // return (
+    //     <div>
+    //         <iframe
+    //             src={'https://docs.google.com/gview?url=' + src + '&embedded=true'}
+    //             sandbox='allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation'
+    //             allow="fullscreen"
+    //             allowFullScreen="allow"
+    //             title="file"
+    //             width={width || '100%'}
+    //             style={{ minHeight: height || '800px', maxHeight: '2000px' }}
+    //         // style={{ Height: '450px', maxHeight: '450px', overflowY: 'scroll' }}
+    //         ></iframe>
+    //     </div>
+    // )
 }
 
 export default DocViewerComponent;
